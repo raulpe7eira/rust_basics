c5bb3a3f3445d233f165a332b57b4f9cffa0351e
Raul Pereira <raul.pereira@gmail.com>
2024-11-20 22:36:27 -0300

    pattern matching

--- main.rs
+++ main.rs
@@ -125,6 +125,18 @@ fn ownership() {
     println!("{}", one_string);
 }
 
+fn pattern_matching() {
+    for x in 1..=20 {
+        println!("{}: {}", x, match x {
+            1 => "Pouco",
+            2 | 3 => "Um pouquinho",
+            4..=10 => "Um bocado",
+            _ if x % 2 == 0 => "Uma boa quantidade",
+            _ => "Muito"
+        });
+    }
+}
+
 fn main() {
     scope();
     shadow();
@@ -132,4 +144,5 @@ fn main() {
     conditionals();
     loops();
     ownership();
+    pattern_matching();
 }
